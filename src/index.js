7eadb72b0cbd6deb0720d06093d8879893a91d02
Panu Leppäniemi <me@panuleppaniemi.com>
2022-04-15 16:51:43 +0300

    Switch to Plasmic

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import {
+  initPlasmicLoader,
+  PlasmicRootProvider,
+  PlasmicComponent,
+} from '@plasmicapp/loader-react'
+
+const PLASMIC = initPlasmicLoader({
+  projects: [
+    {
+      id: process.env.REACT_APP_PLASMIC_ID,
+      token: process.env.REACT_APP_PLASMIC_TOKEN,
+    },
+  ],
+  preview: process.env.REACT_APP_PLASMIC_PREVIEW || false,
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <PlasmicRootProvider loader={PLASMIC}>
+    <PlasmicComponent component="Root" />
+  </PlasmicRootProvider>
+)
