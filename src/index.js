4be71c341830a4671a1ca64fcaaaaa31ac3afcc6
Panu Leppäniemi <me@panuleppaniemi.com>
2022-04-15 20:30:00 +0300

    Loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 
 import {
@@ -7,7 +7,8 @@ import {
   PlasmicComponent,
 } from '@plasmicapp/loader-react'
 
-const PLASMIC = initPlasmicLoader({
+const pageInPlasmic = 'Root'
+const loader = initPlasmicLoader({
   projects: [
     {
       id: process.env.REACT_APP_PLASMIC_ID,
@@ -17,8 +18,32 @@ const PLASMIC = initPlasmicLoader({
   preview: process.env.REACT_APP_PLASMIC_PREVIEW || false,
 })
 
+const Page = () => {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    ;(async function load() {
+      const dataFromPlasmic = await loader.maybeFetchComponentData(
+        pageInPlasmic
+      )
+
+      setData(dataFromPlasmic)
+    })()
+  }, [])
+
+  if (!data) {
+    return (
+      <div id="loader">
+        <div></div>
+      </div>
+    )
+  }
+
+  return <PlasmicComponent component={pageInPlasmic} />
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <PlasmicRootProvider loader={PLASMIC}>
-    <PlasmicComponent component="Root" />
+  <PlasmicRootProvider loader={loader}>
+    <Page />
   </PlasmicRootProvider>
 )
